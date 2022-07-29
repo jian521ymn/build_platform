bd81e0decf7c8cb00a0ecc3ec5ab6895ac423b2a
wb_jian <wb_jian@meituan.com>
2022-07-29 20:26:19 +0800

    1

--- src/pages/productCalendar/index.jsx
+++ src/pages/productCalendar/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Badge, Calendar } from 'antd';
 import "./index.css"
+import moment from 'moment';
 import 'moment/locale/zh-cn';
+import { productRecord } from '../../api/product';
 
 
 const getMonthData = (value) => {
@@ -11,6 +13,19 @@ const getMonthData = (value) => {
 };
 
 const ProductCalendar = () => {
+  const [value, setValue] = useState(moment());
+  const [data, setData] = useState({});
+  useEffect(() => {
+    productRecord({date:`${moment().year()}${moment().month()<10 ? '0' + (moment().month()+1) : moment().month()+1}`}).then(res=>{
+      const obj =res?.data?.list.reduce((prev, next) => {
+        const key =Number(next.date.slice(6,8))
+          prev[key] = Array.from(new Set([...(prev[key] || []),next.product_id]));
+          return prev
+      },{})
+      setData(obj)
+      console.log(obj,'obj');
+    })
+  },[])
   const monthCellRender = (value) => {
     const num = getMonthData(value);
     return num ? (
@@ -22,19 +37,31 @@ const ProductCalendar = () => {
   };
 
   const dateCellRender = (value) => {
-    console.log(value.date(),'value');
+    const list = data[value.date()] || []
     return (
       <ul className="events">
-        {[{}].map((item) => (
+        {list.map((item) => (
           <li key={item.content}>
-            <Badge status='success' text={item.content} />
+            <Badge status='success' text={item} />
           </li>
         ))}
       </ul>
     );
   };
 
-  return <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />;
+  return <Calendar 
+    value={value} 
+    dateCellRender={dateCellRender} 
+    monthCellRender={monthCellRender} 
+    onChange={(val)=>{
+      console.log(val.month(),value.month(),val.year(),value.year());
+      if(val.month() !== value.month() || val.year() !== value.year()){
+
+      }
+      setValue(val)
+
+    }}
+    />;
 };
 
 export default ProductCalendar;
